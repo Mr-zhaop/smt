12dfb602918bb86dd32e27caf3c5e34cfd31e24b
Martin <935557354@qq.com>
2019-10-22 10:12:06 +0800

    first commit

---
+++ view/aa.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="aa"></div>
+</template>
+
+<script>
+export default {
+  name: 'aa',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped></style>
